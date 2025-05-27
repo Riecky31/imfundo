4ddc274c1195c504c0d30835ae3830aa2451da6e
Riecky31 <127891943+Riecky31@users.noreply.github.com>
2025-05-27 21:23:00 +0200

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
 import Header from '@/components/Header'; 
 import About from '@/components/about'; 
 import Body from '@/components/body'; 
-import Modules from '@/components/modules';
+import { listSubject } from '@/components/Data/subject';
 
 
 
 
 export default function Home() {
   return (
-  
+    <div>
     <div style={{color: 'darkblue', alignItems: 'center', }}>
 
       <Header />
       <About /> 
       <Body />
-      <Modules />
-     
+
     </div>
+    <div className="border border-black-300 p-4">
+    { listSubject.map(function (subject){
+          return <p>{subject.title} </p>
+         }) }
+  </div>
+   </div>
+
     
     );
 }
